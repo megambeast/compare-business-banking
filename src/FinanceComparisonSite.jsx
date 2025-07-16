34149ef87490688e4f8a79c3682a9fb4ab4260ee
megambeast <michaellieu8@gmail.com>
2025-07-16 21:12:17 +1000

    Add files via upload

---
+++ src/FinanceComparisonSite.jsx
@@ -0,0 +1,5 @@
+// Paste your full component code here when editing locally or after upload to GitHub
+// This placeholder avoids ZIP overload for now
+export default function FinanceComparisonSite() {
+  return <div style={{ padding: 40 }}>Replace this with your full component</div>;
+}
